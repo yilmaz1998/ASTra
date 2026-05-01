ab4dda8be3294d4914186660f6cc3fa5ec97540a
yilmaz1998 <snn.ylmz9804@gmail.com>
2026-05-01 09:31:38 -0400

    backend server set up

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+}
+);
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
